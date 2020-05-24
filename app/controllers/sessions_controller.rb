ab0edf7eea09e95e41f9d47d0b3dd7e33ec34085
James Ridgway <james@jamesridgway.co.uk>
2020-05-24 12:18:05 +0100

    OTP login as second step

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < Devise::SessionsController
+  include AuthenticateWithOtpTwoFactor
   layout 'login'
 
-  protected
+  prepend_before_action :authenticate_with_otp_two_factor,
+                        if: -> { action_name == 'create' && otp_two_factor_enabled? }
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
-  end
+  protect_from_forgery with: :exception, prepend: true, except: :destroy
 
 end
--- spec/features/login/user_can_login_with_otp_spec.rb
+++ spec/features/login/user_can_login_with_otp_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 feature 'User with OTP two factor enabled' do
+  scenario 'cannot login without unknown user' do
+    # When I enter a username and password that does not exist
+    visit root_path
+    fill_in 'Email', with: 'someone@example.com'
+    fill_in 'Password', with: 'letmein'
+    click_button 'Login'
+
+    # Then I expect to see an error message
+    expect(page).to have_content("Invalid Email or password.")
+  end
   scenario 'cannot login without a valid OTP' do
     # Given I am a user that has OTP two factor authentication enabled
     user = create(:user, :with_otp)
@@ -11,8 +21,12 @@ feature 'User with OTP two factor enabled' do
     fill_in 'Password', with: 'letmein'
     click_button 'Login'
 
+    # And I am then prompted for my OTP and I provide an invalid OTP
+    fill_in 'OTP', with: 'invalid-otp'
+    click_button 'Login'
+
     # I expect to see an error message
-    expect(page).to have_content("Invalid Email or password.")
+    expect(page).to have_content("Invalid two-factor code.")
   end
   scenario 'can login when providing a valid OTP' do
     # Given I am a user that has OTP two factor authentication enabled
@@ -22,6 +36,9 @@ feature 'User with OTP two factor enabled' do
     visit root_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'letmein'
+    click_button 'Login'
+
+    # And I am then prompted for my OTP
     fill_in 'OTP', with: user.current_otp
     click_button 'Login'
 
